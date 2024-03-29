db2a54019debab41c2862edda881a0f57e3301fc
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-03-29 15:30:49 +0000

    try to complile

---
+++ src/tutorial.ts
@@ -0,0 +1,14 @@
+console.log('Hello TypeScript!');
+
+interface someValue {
+  name: string;
+  id: number;
+}
+
+let someObj: someValue = {
+  name: 'random',
+  id: 123,
+};
+
+
+console.log(someObj);
